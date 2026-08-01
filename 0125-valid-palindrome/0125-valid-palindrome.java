7787394565b094b2588636d9908afd5c0bb0fc1c
Abhishek Pal <abhishekpal.074111@gmail.com>
2026-08-02 03:04:56 +0530

    Time: 3 ms (58.5%), Space: 44.2 MB (74.81%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,33 @@
+class Solution 
+{
+    public boolean isPalindrome(String s) 
+    {
+        // Convert to lowercase
+        s = s.toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) 
+        {
+            // Skip non-alphanumeric characters
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) 
+            {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) 
+            {
+                right--;
+            }
+            
+            // Compare characters
+            if (s.charAt(left) != s.charAt(right)) 
+            {
+                return false;
+            }
+            
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
