a8027c8e93251ce3b40d92cc0f7727a298f173c2
Abhishek Pal <abhishekpal.074111@gmail.com>
2026-09-04 03:35:19 +0530

    README.md

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -26,31 +26,26 @@ class Solution
             return false;
         }
     }
-
     private int height(TreeNode root) 
     {
         if (root == null) 
         {
             return 0;
         }
-
         int left = height(root.left);
         if (left == -1) 
         {
             return -1;
         }
-
         int right = height(root.right);
         if (right == -1) 
         {
             return -1;
         }
-
         if (Math.abs(left - right) > 1) 
         {
             return -1;
         }
-
         return Math.max(left, right) + 1;
     }
 }
