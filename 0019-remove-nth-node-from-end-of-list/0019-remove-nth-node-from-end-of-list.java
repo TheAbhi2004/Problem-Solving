885461961a31dd428f3e42c411e3c43cf096c43a
Abhishek Pal <abhishekpal.074111@gmail.com>
2026-09-04 03:30:26 +0530

    README.md

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -4,15 +4,12 @@ class Solution
     {
         ListNode dummy=new ListNode(0);
         dummy.next=head;
-
         ListNode slow=dummy;
         ListNode fast=dummy;
-
         for(int i=0;i<n;i++)
         {
             fast=fast.next;
         }
-
         while(fast.next!=null)
         {
             slow=slow.next;
