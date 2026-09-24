48cbc5cf36c11f22ba75f3542edf473b7405224d
Abhishek Pal <abhishekpal.074111@gmail.com>
2026-09-25 00:33:48 +0530

    Time: 1 ms (99.69%), Space: 45.6 MB (40.74%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -5,7 +5,7 @@ class Solution
         for(int i=0;i<nums.length;i++)
         {
             int n=nums[i];
-            int sum = 0;
+            int sum=0;
             while(n>0)
             {
                 int r=n%10;
