d9c75290ceeda4fe5d721d2dee137b9eb71fbb4d
Abhishek Pal <abhishekpal.074111@gmail.com>
2026-09-26 02:08:14 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.31%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,3 +1,5 @@
+/*
+Recursive
 class Solution 
 {
     public int fib(int n) 
@@ -10,4 +12,25 @@ class Solution
         int a2=fib(n-2);
         return a1+a2;
     }
+}*/ 
+
+//Solving using DP for fast computation
+class Solution
+{
+    public int fib(int n)
+    {
+        if(n==0||n==1)
+        {
+            return n;
+        }
+        int[] dp=new int[n+1];
+        dp[0]=0;dp[1]=1;
+        int i;
+        for(i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
 }
+
