22c40163882603cde94a5e70b9caf8b238cab499
Abhishek Pal <abhishekpal.074111@gmail.com>
2026-09-26 02:01:14 +0530

    Time: 10 ms (25.69%), Space: 42.2 MB (16.31%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution 
+{
+    public int fib(int n) 
+    {
+        if(n==0||n==1)
+        {
+            return n;
+        }
+        int a1=fib(n-1);
+        int a2=fib(n-2);
+        return a1+a2;
+    }
+}
